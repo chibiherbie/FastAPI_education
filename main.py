929196426760cefa4be3d35c6f35ad6efe588dc9
Roman <bekkerrdm@gmail.com>
2024-09-15 15:12:28 +0700

    add route GET /hotels

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+hotels = [
+    {"id": 1, "title": 'Sochi'},
+    {"id": 2, "title": "Dubai"}
+]
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/hotels")
+def get_hotels(
+        id: int | None = Query(None, description='Айдишник'),
+        title: str | None = Query(None, description='Название отеля'),
+):
+    _hotels = []
+    for hotel in hotels:
+        if id and id != hotel["id"]:
+            continue
+        if title and title != hotel["title"]:
+            continue
+        _hotels.append(hotel)
+    return _hotels
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", reload=True)
