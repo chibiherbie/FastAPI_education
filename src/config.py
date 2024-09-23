56b97c51fc61f000ffda228e894fd766f6180686
Roman <bekkerrdm@gmail.com>
2024-09-23 17:57:20 +0700

    feat(database): create database.py and add DB_URL to settings

--- src/config.py
+++ src/config.py
@@ -2,9 +2,17 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
     DB_NAME: str
 
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    @property
+    def DB_URL(self):  # noqa
+        return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
--- src/main.py
+++ src/main.py
@@ -7,9 +7,7 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
 from src.api.hotels import router as router_hotels
-from src.config import settings
-
-print(settings.DB_NAME)
+from src.db import *
 
 app = FastAPI()
 app.include_router(router_hotels)
